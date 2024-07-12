fee2556edfc7cd73ad231ec5c88d5627a1d9f35d
jjaw <jjaw@jjaw.cn>
2024-07-12 16:30:19 +0800

    页面生成ok

---
+++ vuepress.config.ts
@@ -0,0 +1,10 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defineUserConfig } from 'vuepress'
+import { FileList } from './src/node/index.js'
+
+export default defineUserConfig({
+  bundler: viteBundler(),
+  theme: FileList([
+    {path:"",user:"jianjianai",repository:"alist-github-releases-files"}
+  ]),
+})
