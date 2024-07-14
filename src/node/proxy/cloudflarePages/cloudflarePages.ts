c1277ff387777cf0cdaa328cad859f370f1c0ec3
jjaw <jjaw@jjaw.cn>
2024-07-14 19:23:34 +0800

    下载代理规则修改

--- src/node/proxy/cloudflarePages/cloudflarePages.ts
+++ src/node/proxy/cloudflarePages/cloudflarePages.ts
@@ -3,7 +3,22 @@ import {writeFile} from "fs";
 import {path} from "vuepress/utils";
 import {downloadProxy} from "./worker/cloudflarePagesDownloadPorxy.js";
 
+
+
+function hashCode(string:string):number {
+    let hash = 0, i, chr;
+    if (string.length === 0) return hash;
+    for (i = 0; i < string.length; i++) {
+        chr = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+
 const includes:string[] = [];
+const proxyConfig:{[path:string]:string} = {}
 /**
  * 代理这些文件的下载并生成下载允许的路由列表
  * */
@@ -16,12 +31,14 @@ export function getDownProxyRoutes(){
 }
 
 export const cloudflarePagesDownProxy:DownProxy = async (sourceUrl:string):Promise<string>=>{
-    const downProxyPath = `/down/${sourceUrl}`;
-    includes.push(downProxyPath);
+    const downProxyPath = `/down/${hashCode(sourceUrl)}/${sourceUrl.substring(sourceUrl.lastIndexOf("/")+1)}`;
+    const routerPath = `/down/${hashCode(sourceUrl)}/*`;
+    proxyConfig[downProxyPath] = sourceUrl;
+    includes.push(routerPath);
     return downProxyPath;
 }
 
 export async function cloudflarePagesReleaseConfigurationFile(destPath:string){
     await new Promise((r)=>writeFile(path.join(destPath,"_routes.json"),JSON.stringify(getDownProxyRoutes()),r));
-    await new Promise((r)=>writeFile(path.join(destPath,"_worker.js"),`${downloadProxy.toString()}\nexport default { fetch:downloadProxy };`,r));
+    await new Promise((r)=>writeFile(path.join(destPath,"_worker.js"),`${downloadProxy.toString()}\nconst proxyConfig = ${JSON.stringify(proxyConfig)}\nexport default {fetch:(req)=>downloadProxy(req,proxyConfig)};`,r));
 }
--- src/node/proxy/cloudflarePages/worker/cloudflarePagesDownloadPorxy.ts
+++ src/node/proxy/cloudflarePages/worker/cloudflarePagesDownloadPorxy.ts
@@ -1,31 +1,35 @@
-export async function downloadProxy(request: Request): Promise<Response> {
-    const prefix = "/down/";
+export async function downloadProxy(request: Request,proxyConfig:{[path:string]:string}): Promise<Response> {
     const requestUrl = new URL(request.url);
-    let downUrlString = requestUrl.pathname+requestUrl.search;
-    if(downUrlString.startsWith(prefix)){
-        downUrlString = downUrlString.substring(prefix.length);
-    }else{
-        return new Response("download prefix not");
+    let downUrlString = proxyConfig[requestUrl.pathname];
+    if(!downUrlString){
+        return new Response("not found",{status:404});
     }
     let downUrl:URL;
     try{
         downUrl = new URL(downUrlString);
     }catch(error){
-        return new Response("url error: "+error);
+        return new Response("url error: "+error,{status:404});
     }
-    const res = await fetch(downUrl,request);
-    const headers = new Headers(res.headers);
-
-    if (res.status >= 300 && res.status < 400) {
-        const loto = headers.get("Location");
-        if(loto){
-            let goURL = new URL(loto,downUrl);
-            headers.set("Location",requestUrl.origin+prefix+goURL);
+    let res = await fetch(downUrl,request);
+    let count = 0;
+    let lastUrl = downUrl;
+    while (true){
+        count++;
+        if(count>10){
+            return new Response("redirect too many",{status:500});
         }
+        const headers = new Headers(res.headers);
+        if (res.status >= 300 && res.status < 400) {
+            const loto = headers.get("Location");
+            if(loto){
+                lastUrl = new URL(loto,lastUrl);
+                res = await fetch(lastUrl,request);
+                continue;
+            }
+        }
+        return new Response(res.body as any,{
+            headers:headers,
+            status:res.status
+        })
     }
-
-    return new Response(res.body as any,{
-        headers:headers,
-        status:res.status
-    })
 }
