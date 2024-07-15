aaecf07f64f87aae13c77a49bfd81c2fb9666eff
jjaw <jjaw@jjaw.cn>
2024-07-15 13:50:00 +0800

    404页面

--- src/client/layouts/NotFound.vue
+++ src/client/layouts/NotFound.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 
+import FilesPageMain from "../components/FilesPageMain.vue";
+import LoadError from "../components/fileView/views/LoadError.vue";
 </script>
 
 <template>
-404
+  <FilesPageMain>
+    <LoadError message="404 文件不存在!"></LoadError>
+  </FilesPageMain>
 </template>
 
 <style scoped>
