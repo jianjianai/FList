4430fc0bdc601e773ebdde06c21df12a29697887
jjaw <jjaw@jjaw.cn>
2024-08-02 19:00:47 +0800

    fetch 配置错误修改

--- src/node/proxy/cloudflarePages/worker/cloudflarePagesDownloadPorxy.ts
+++ src/node/proxy/cloudflarePages/worker/cloudflarePagesDownloadPorxy.ts
@@ -16,25 +16,8 @@ export async function downloadProxy(request: Request,proxyConfig:{[path:string]:
         body:request.body
     }
     let res = await fetch(downUrl,reqConfig);
-    let count = 0;
-    let lastUrl = downUrl;
-    while (true){
-        count++;
-        if(count>10){
-            return new Response("redirect too many",{status:500});
-        }
-        const headers = new Headers(res.headers);
-        if (res.status >= 300 && res.status < 400) {
-            const loto = headers.get("Location");
-            if(loto){
-                lastUrl = new URL(loto,lastUrl);
-                res = await fetch(lastUrl,reqConfig);
-                continue;
-            }
-        }
-        return new Response(res.body as any,{
-            headers:headers,
-            status:res.status
-        })
-    }
+    return new Response(res.body as any,{
+        headers:res.headers,
+        status:res.status
+    })
 }
