5335944eca668296457fb5ecdbad02c13cfb14c3
jjaw <jjaw@jjaw.cn>
2024-09-21 17:00:06 +0800

    修复 vite 开发环境下代理链接挑食的问题。

--- src/node/proxy/vercelDownProxy/index.ts
+++ src/node/proxy/vercelDownProxy/index.ts
@@ -47,7 +47,7 @@ onExtendsBundlerOptions(async (options, app) => {
         for (const proxyConfigKey in proxyConfig) {
             const proxyConfigValue = proxyConfig[proxyConfigKey];
             proxy[proxyConfigKey] = {
-                target: proxyConfigValue,
+                target: new URL(proxyConfigValue).toString(),
                 changeOrigin: true,
                 rewrite: () => "",
                 followRedirects: true,
