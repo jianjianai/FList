7e8cd24b1ca250d74a62c4a5256c462e172e21fb
jjaw <jjaw@jjaw.cn>
2024-09-21 17:51:14 +0800

    FaceDataset 添加隐藏 Readme 选项

--- src/node/analysis/huggingFaceDatasetsAnalysis/index.ts
+++ src/node/analysis/huggingFaceDatasetsAnalysis/index.ts
@@ -1,8 +1,8 @@
-import {addFileToFileTree, Folder, getFileByPath, isFile} from "../../base/files.js";
+import {addFileToFileTree, Folder, getFileByPath} from "../../base/files.js";
 import {Analysis} from "../../base/AllAnalysis.js";
 
 
-async function huggingFaceDatasetsAnalysisTree(fileTree:Folder,userName:string,datasetsName:string,branchName:string,rootPath:string,path:string,hasDeep:number):Promise<Folder>{
+async function huggingFaceDatasetsAnalysisTree(fileTree:Folder,userName:string,datasetsName:string,branchName:string,rootPath:string,path:string,hasDeep:number,hideReadme?: boolean):Promise<Folder>{
     let response;
     try {
         response = await fetch(`https://huggingface.co/api/datasets/${userName}/${datasetsName}/tree/${branchName}${path?"/"+path:path}`);
@@ -24,12 +24,7 @@ async function huggingFaceDatasetsAnalysisTree(fileTree:Folder,userName:string,d
             const rpNum = rootPath.split("/").length;
             pathArray.splice(0,rpNum);
             const url = `https://huggingface.co/datasets/${userName}/${datasetsName}/resolve/${branchName}/${jsonDatum.path}?download=true`;
-            addFileToFileTree(fileTree,pathArray,{
-                name:fileName,
-                downloadUrl:url,
-                size:jsonDatum.size,
-                downloadCorsAllow:"allow" //huggingface的下载链接是允许跨域的
-            });
+            let addThisFile = true;
             if(fileName.toUpperCase()=="README.MD"){
                 const fileDir = getFileByPath(fileTree,pathArray);
                 if(fileDir){
@@ -39,6 +34,17 @@ async function huggingFaceDatasetsAnalysisTree(fileTree:Folder,userName:string,d
                         throw new Error("HuggingFace Api 请求失败! 请检查网络是否畅通。"+error);
                     }
                 }
+                if(hideReadme){
+                    addThisFile = false;
+                }
+            }
+            if(addThisFile){
+                addFileToFileTree(fileTree,pathArray,{
+                    name:fileName,
+                    downloadUrl:url,
+                    size:jsonDatum.size,
+                    downloadCorsAllow:"allow" //huggingface的下载链接是允许跨域的
+                });
             }
         }else if(jsonDatum.type=="directory"){
             if (hasDeep>0){
@@ -55,6 +61,7 @@ export function huggingFaceDatasetsAnalysis(config:{
     branchName:string,
     path?:string
     maxDeep?:number
+    hideReadme?:boolean
 }):Analysis{
     if(config.path && config.path.startsWith("/")){
         config.path = config.path.substring(1);
@@ -62,6 +69,6 @@ export function huggingFaceDatasetsAnalysis(config:{
     return ()=>{
         const fileTree:Folder = {children:[],name:"huggingFaceDatasetsAnalysisRoot"};
         const path = config.path || "";
-        return huggingFaceDatasetsAnalysisTree(fileTree,config.userName,config.datasetsName,config.branchName,path,path,config.maxDeep || 10)
+        return huggingFaceDatasetsAnalysisTree(fileTree,config.userName,config.datasetsName,config.branchName,path,path,config.maxDeep || 10,config.hideReadme)
     };
 }
